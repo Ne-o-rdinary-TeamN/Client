a3ae33d6077e16e9e8170e1626888f7fd521345c
cho4u4o <cho4u4o@gmail.com>
2025-11-23 02:14:12 +0900

    feat: top news 엔드포인트 추가

--- src/shared/config/endpoint.ts
+++ src/shared/config/endpoint.ts
@@ -1,9 +1,12 @@
-export const PUBLIC_API_URL = "";
+export const PUBLIC_API_URL = "http://15.164.233.220:8080";
 
 export const ENDPOINT = {
   AUTH: {
     LOGIN: "",
     JOIN: "",
+  },
+  NEWS: {
+    TOP_NEWS: "/api/search/news?display=5",
   },
   BOARD: {
     GET_POST: (id: string | number) => `/board/${id}`,
