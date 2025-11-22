a7cd555d4bf5f4a267b3b9ccdb4943e43d7af10b
kimkimjunjun <wnsgnl323@gmail.com>
2025-11-22 23:43:55 +0900

    fix: 로고 수정

--- src/page/home/ui/Homepage.tsx
+++ src/page/home/ui/Homepage.tsx
@@ -4,40 +4,41 @@ import HotTopic from "./HotTopic";
 import Footer from "@/widgets/layout/Footer/Footer";
 import Image from "next/image";
 import Link from "next/link";
+import Logo from "../../../../public/icons/Logo.svg";
 
 function Homepage() {
-  return (
-    <>
-      <div className="flex flex-col h-screen overflow-y-auto relative">
-        <Image
-          src="/icons/logo.svg"
-          alt="logo"
-          width={100}
-          height={100}
-          className="ml-4 mt-4"
-        />
-        <HotTopic />
-        <HotNews />
-        <Link
-          href="/write"
-          className="fixed bottom-[80px] right-side active:brightness-90"
-        >
-          <div className="px-[18px] py-3 bg-blue-600 rounded-3xl shadow-[0px_4px_10px_0px_rgba(0,0,0,0.15)] inline-flex justify-center items-center gap-2">
-            <Image
-              src="/icons/article.svg"
-              alt="article"
-              width={16}
-              height={16}
-            />
-            <div className="text-center justify-center text-white font-semibold-16 leading-5">
-              글쓰기
+    return (
+        <>
+            <div className="flex flex-col h-screen overflow-y-auto relative">
+                <Image
+                    src={Logo}
+                    alt="logo"
+                    width={114}
+                    height={30}
+                    className="ml-4 mt-5"
+                />
+                <HotTopic />
+                <HotNews />
+                <Link
+                    href="/write"
+                    className="fixed bottom-[80px] right-side active:brightness-90"
+                >
+                    <div className="px-[18px] py-3 bg-blue-600 rounded-3xl shadow-[0px_4px_10px_0px_rgba(0,0,0,0.15)] inline-flex justify-center items-center gap-2">
+                        <Image
+                            src="/icons/article.svg"
+                            alt="article"
+                            width={16}
+                            height={16}
+                        />
+                        <div className="text-center justify-center text-white font-semibold-16 leading-5">
+                            글쓰기
+                        </div>
+                    </div>
+                </Link>
             </div>
-          </div>
-        </Link>
-      </div>
-      <Footer />
-    </>
-  );
+            <Footer />
+        </>
+    );
 }
 
 export default Homepage;
