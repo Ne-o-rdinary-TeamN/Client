2ac4ab27f3131b66ec1311a3ad187a9e2fd27c8f
cho4u4o <cho4u4o@gmail.com>
2025-11-22 21:02:58 +0900

    feat : 가입 페이지 구현 완료

---
+++ src/page/join/ui/JoinStep.tsx
@@ -0,0 +1,25 @@
+import { Button, TextField } from "@/shared/ui";
+import JoinStepLayout from "./JoinStepLayout";
+
+interface JoinStepProps {
+  type: "name" | "email" | "password";
+  placeholder: string;
+  onNext: (value: string) => void;
+}
+
+export default function JoinStep({ type, placeholder, onNext }: JoinStepProps) {
+  return (
+    <JoinStepLayout type={type}>
+      <TextField
+        placeholder={placeholder}
+        type={type === "password" ? "password" : "text"}
+      />
+      <div className="flex-1 flex flex-col justify-end">
+        <div className="flex w-full items-center gap-2">
+          {type !== "name" && <JoinStepLayout.BackButton />}
+          <Button text="다음" className="self-end" onClick={() => onNext("")} />
+        </div>
+      </div>
+    </JoinStepLayout>
+  );
+}
